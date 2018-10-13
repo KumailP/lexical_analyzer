ac2a17532b55154192fc6953602a3f87d0f3dd87
Kumail Pirzada <kumailpirzada@gmail.com>
2018-10-13 17:53:25 +0100

    Added initial structure of lexical analyzer

---
+++ lexicalAnalyzer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Lexer
+{
+  public:
+    ifstream s;
+    char next;
+
+    Lexer(string _s)
+    {
+        s.open(_s);
+        s.get(next);
+        cout << "Value of next is: " << next;
+    }
+};
+
+int main()
+{
+    string inputFileName = "test.txt";
+    Lexer lexer(inputFileName);
+    return 0;
+}
